ba65bf91c8da7cc3348a7081314831ebe2954a87
anuzx <anujparasar4266@gmail.com>
2025-11-01 19:19:10 +0530

    redirect from signup to signin

--- learning/app/signup/page.tsx
+++ learning/app/signup/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Signin() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="border p-2">
@@ -27,11 +29,12 @@ export default function Signin() {
         ></input>
         <br />
         <button
-          onClick={() => {
-            axios.post("http://localhost:3000/api/v1/signup", {
+          onClick={async () => {
+            await axios.post("http://localhost:3000/api/v1/signup", {
               username,
               password,
             });
+            router.push("/signin");
           }}
         >
           Sign Up
